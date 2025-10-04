1d36f806f2cf6c168dee68f2f1eaaeea1faf16c3
Juan <jjmontoya0528@gmail.com>
2025-10-04 18:43:10 -0500

    Creación del SizeDomain

---
+++ ApiRapizzas/src/main/java/co/edu/uco/rapizzas/business/domain/SizeDomain.java
@@ -0,0 +1,44 @@
+package co.edu.uco.rapizzas.business.domain;
+
+import java.util.UUID;
+
+import co.edu.uco.rapizzas.crosscuting.helper.ObjectHelper;
+import co.edu.uco.rapizzas.crosscuting.helper.TextHelper;
+import co.edu.uco.rapizzas.crosscuting.helper.UUIDHelper;
+
+public class SizeDomain extends Domain{
+	
+	private String nameSize;
+	
+	public SizeDomain() {
+		super(UUIDHelper.getUUIDHelper().getDefault());
+		setNameSize(TextHelper.getDefault());	
+	}
+	
+	public SizeDomain(final UUID id) {
+		super(id);
+		setNameSize(TextHelper.getDefault());	
+	}
+	
+	public SizeDomain(final UUID id, final String nameSize) {
+		super(id);
+		setNameSize(nameSize);
+	}
+	
+	static SizeDomain getDefaultValue() {
+		return new SizeDomain();
+	}
+	
+	static SizeDomain getDefaultValue(final SizeDomain size) {
+		return ObjectHelper.getDefault(size, getDefaultValue());
+	}
+
+	public String getNameSize() {
+		return nameSize;
+	}
+
+	public void setNameSize(final String nameSize) {
+		this.nameSize = TextHelper.getDefaultWithTrim(nameSize);
+	}
+	
+}

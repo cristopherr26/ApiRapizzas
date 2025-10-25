5eb99e86e90180a57a948f693e55f9037a1bffd2
Juan <jjmontoya0528@gmail.com>
2025-10-25 13:47:55 -0500

    Creación de clases y paquetes vistos en la Sesión 14

---
+++ ApiRapizzas/src/main/java/co/edu/uco/rapizzas/controller/EmployeeController.java
@@ -0,0 +1,10 @@
+package co.edu.uco.rapizzas.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/employees")
+public class EmployeeController {
+
+}
---
+++ ApiRapizzas/src/main/java/co/edu/uco/rapizzas/controller/dto/Response.java
@@ -0,0 +1,11 @@
+package co.edu.uco.rapizzas.controller.dto;
+
+import java.util.List;
+
+public final class Response<T> {
+	
+	private List<String> messages;
+	private List<String> data;
+	private boolean responseSucceded;
+
+}
